be0cdf10285dc69e7255f0fe4477547094e5969e
Andrew <haxerwf@yandex.ru>
2023-03-11 11:24:10 +0300

    fixed something

--- NewsPortal_search_problem/NewsPortal/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import NewsList, NewsDetail, NewsSearch
-
-urlpatterns = [
-    path('', NewsList.as_view()),
-    path('<int:pk>', NewsDetail.as_view()),
-    path('search/', NewsSearch.as_view(), name='search'),
- ]
